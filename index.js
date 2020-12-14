b478cc0f17e6c2b1f8025e46427347f92b43b31c
LJ <jhlee9193@gmail.com>
2020-12-14 21:11:54 +0900

    Overview to the final API

--- index.js
+++
@@ -1,9 +0,0 @@
-import { GraphQLSever } from "graphql-yoga";
-import resolvers from "./graphql/resolvers";
-
-const server = new GraphQLSever({
-  typeDefs: "graphql/schema.graphql",
-  resolvers,
-});
-
-server.start(() => console.log("Graphql Sever Running"));
